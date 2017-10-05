cbd7a401b3241e95aafe95bc1dc33c4990635d32
balzhan <balka8406@gmail.com>
2017-10-05 12:45:42 +0600

    experiment1 бұтағында Hello.java файылына тағы өзгерістер жасалынды

--- src/qwerty/Hello.java
+++ src/qwerty/Hello.java
@@ -6,10 +6,10 @@ System.out.print("Hello World!");
 System.out.print("Бұл зертхналық жұмыстың мақсаты:");
         System.out.print("GitGub жұмыс жасап үйрену.");
 
-        int a = MyMath.kvadrat ( 5 );
-        int b = MyMath.kub (  5 );
-
-        System.out.println("a = " + a);
+        System.out.println("max = " MyMath.max3(a:8, b:9, c:12));
         System.out.println("b = " + b);
+
+        System.out.println(MyMath.max3( 8, 9,  12 ));
+        System.out.println(MyMath.min3( 8, 9,  12 ));
     }
 }
