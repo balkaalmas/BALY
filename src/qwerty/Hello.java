a504067774af7e1b46e6821159bda5a783b97ba2
balzhan <balka8406@gmail.com>
2017-10-05 11:53:58 +0600

    NTP sabagy

--- src/qwerty/Hello.java
+++ src/qwerty/Hello.java
@@ -2,6 +2,8 @@ package qwerty;
 
 public class Hello {
     public static void main(String[] args) {
-System.out.print("Hello World");
+System.out.print("Hello World!");
+System.out.print("Бұл зертхналық жұмыстың мақсаты:");
+        System.out.print("GitGub жұмыс жасап үйрену.");
     }
 }
