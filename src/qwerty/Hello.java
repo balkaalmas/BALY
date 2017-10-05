dd30256f067b3761cfc8f9fe80ea4090fd17d020
balzhan <balka8406@gmail.com>
2017-10-05 12:13:58 +0600

    Үшінші коммит

--- src/qwerty/Hello.java
+++ src/qwerty/Hello.java
@@ -5,5 +5,11 @@ public class Hello {
 System.out.print("Hello World!");
 System.out.print("Бұл зертхналық жұмыстың мақсаты:");
         System.out.print("GitGub жұмыс жасап үйрену.");
+
+        int a = MyMath.kvadrat ( 5 );
+        int b = MyMath.kub (  5 );
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
     }
 }
